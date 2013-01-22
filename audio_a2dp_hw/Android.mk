63e7a71af0cf1d98ec11f8df75cc398843da6002
Steve Kondik <shade@chemlab.org>
2013-01-22 13:21:26 -0800

    Conditional compilation for 48KHz sample rate
    PS2:
    https://github.com/CyanogenMod/android_external_bluetooth_bluedroid/commit/2cbe5233895e03255d655044897b9a27fcfc4e51
    All QC devices using the CodeAurora drivers should be using a 48KHz
    samplerate for A2DP.
    
    Change-Id: I0a439973605369509efd06486181007978627414

--- audio_a2dp_hw/Android.mk
+++ audio_a2dp_hw/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-bfam)
+	LOCAL_CFLAGS += -DSAMPLE_RATE_48K
+endif
+
+LOCAL_SRC_FILES:= \
 	audio_a2dp_hw.c
 
 LOCAL_C_INCLUDES += \

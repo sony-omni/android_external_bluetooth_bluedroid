codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bluetooth: Added NULL checks to avoid NULL pointer dereferences
This patch fixed the NULL pointer dereferences and Array
Index Out of Bounds exceptions seen in the code space of
bluedroid in Bluetooth.

CRs-Fixed: 792272
Change-Id: Id8775d9d14efb4d52c3cee26e7a2e77a22bcf3ba

btif/src/btif_rc.c
@@ -1188,7 +1188,7 @@ UINT8 handle_get_folder_item_filesystem_cmd (tBTA_AV_BROWSE_MSG *pbrowse_msg, tA
     {
         BTIF_TRACE_DEBUG("No attribute requested");
     }
-    else
+    else if (attr_count <= AVRC_MAX_ELEM_ATTR_SIZE)
     {
         p_data = &pbrowse_msg->p_msg->browse.p_browse_data[13];
         for (xx = 0; xx < attr_count; xx++)
@@ -2462,7 +2462,7 @@ static bt_status_t get_player_app_value_rsp(btrc_player_settings_t *p_vals, bt_b
     {
         avrc_rsp.get_cur_app_val.status = AVRC_STS_BAD_PARAM;
     }
-    else
+    else if (p_vals->num_attr <= AVRC_MAX_APP_ATTR_SIZE)
     {
         memset(app_sett, 0, sizeof(tAVRC_APP_SETTING)*p_vals->num_attr );
         //update num_val

sony-omni/android_external_bluetooth_bluedroid

btif/src/btif_rc.c
@@ -974,7 +974,7 @@ UINT8 handle_get_folder_item_filesystem_cmd (tBTA_AV_BROWSE_MSG *pbrowse_msg, tA
     {
         BTIF_TRACE_DEBUG("No attribute requested");
     }
-    else
+    else if (attr_count <= AVRC_MAX_ELEM_ATTR_SIZE)
     {
         p_data = &pbrowse_msg->p_msg->browse.p_browse_data[13];
         for (xx = 0; xx < attr_count; xx++)
@@ -2129,7 +2129,7 @@ static bt_status_t get_player_app_value_rsp(btrc_player_settings_t *p_vals)
     {
         avrc_rsp.get_cur_app_val.status = AVRC_STS_BAD_PARAM;
     }
-    else
+    else if (p_vals->num_attr <= BTRC_MAX_APP_SETTINGS)
     {
         memset(app_sett, 0, sizeof(tAVRC_APP_SETTING)*p_vals->num_attr );
         //update num_val